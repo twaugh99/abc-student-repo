d5a163fa44df577ceb01c7935210fd1dfd80d9e6
Thomas <tsw298@nyu.edu>
2021-11-10 14:41:54 -0600

    .

--- my-room/script.js
+++ my-room/script.js
@@ -7,9 +7,9 @@ let range = document.getElementById('myRange');
 
 
 //Add Project Links Here
-linkArray = ["","https://twaugh99.github.io/abc-student-repo/projects/multiplicity-project", "https://twaugh99.github.io/abc-student-repo/projects/time-project", "", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/bug-extension.zip", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/gift_project.zip", "", "", "", "", "", "https://twaugh99.github.io/abc-student-repo/projects/project-A", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/project-B/README.md", ""]
-iconArray = ["","assets/multiplicityIcon.png", "assets/timeIcon.png", "", "assets/bugIcon.png", "assets/202020icon.png", "", "", "", "", "", "assets/projectAIcon.png", "assets/swipeySynthIcon.png", ""]
-titleArray = ["","Multiplicity Project  (9/9/21)", "Time Project  (9/9/21)", "", "Bug Extension (10/12/21)", "20-20-20 Extension (10/21/21)", "", "", "", "", "", "Project A  (9/30/21)", "SwipeySynth (10/27/21)", ""]
+linkArray = ["","https://twaugh99.github.io/abc-student-repo/projects/multiplicity-project", "https://twaugh99.github.io/abc-student-repo/projects/time-project", "", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/bug-extension.zip", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/gift_project.zip", "https://ipa-riddle.glitch.me/", "", "", "", "", "https://twaugh99.github.io/abc-student-repo/projects/project-A", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/project-B/README.md", ""]
+iconArray = ["","assets/multiplicityIcon.png", "assets/timeIcon.png", "", "assets/bugIcon.png", "assets/202020icon.png", "assets/ipa-riddle-icon.png", "", "", "", "", "assets/projectAIcon.png", "assets/swipeySynthIcon.png", ""]
+titleArray = ["","Multiplicity Project  (9/9/21)", "Time Project  (9/9/21)", "", "Bug Extension (10/12/21)", "20-20-20 Extension (10/21/21)", "IPA Riddle (11/11/21)", "", "", "", "", "Project A  (9/30/21)", "SwipeySynth (10/27/21)", ""]
 
 
 function inputHappened(){
