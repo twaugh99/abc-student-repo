554d1ba39fec88e2b323d0e03f1bca3a573a5aad
Thomas <tsw298@nyu.edu>
2021-11-17 13:41:03 -0600

    vacuumed my room

--- my-room/script.js
+++ my-room/script.js
@@ -1,75 +1,85 @@
 console.log('script loaded');
 
+let previewText = document.getElementById("previewText");
 
-//Add Project Links Here
-// let linkArray = ["","https://twaugh99.github.io/abc-student-repo/projects/multiplicity-project", "https://twaugh99.github.io/abc-student-repo/projects/time-project", "", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/bug-extension.zip", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/gift_project.zip", "https://ipa-riddle.glitch.me/", "", "", "", "", "https://twaugh99.github.io/abc-student-repo/projects/project-A", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/project-B/README.md", ""]
-// let iconArray = ["","assets/multiplicityIcon.png", "assets/timeIcon.png", "assets/placeholderIcon.png", "assets/bugIcon.png", "assets/202020icon.png", "assets/ipa-riddle-icon.png", "assets/placeholderIcon.png", "assets/placeholderIcon.png", "assets/placeholderIcon.png", "assets/placeholderIcon.png", "assets/projectAIcon.png", "assets/swipeySynthIcon.png", "assets/placeholderIcon.png"]
-// let titleArray = ["","Multiplicity Project  (9/9/21)", "Time Project  (9/9/21)", "", "Bug Extension (10/12/21)", "20-20-20 Extension (10/21/21)", "IPA Riddle (11/11/21)", "", "", "", "", "Project A  (9/30/21)", "SwipeySynth (10/27/21)", ""]
-
-miniProject1.addEventListener("click", ()=>{
-  window.location.href = "https://twaugh99.github.io/abc-student-repo/projects/multiplicity-project";
-})
-
-miniProject2.addEventListener("click", ()=>{
-  window.location.href = "https://twaugh99.github.io/abc-student-repo/projects/time-project";
-})
-
-miniProject3.addEventListener("click", ()=>{
-  // window.location.href = "";
-})
-
-miniProject4.addEventListener("click", ()=>{
-  window.location.href = "https://github.com/twaugh99/abc-student-repo/blob/master/projects/bug-extension.zip";
-})
-
-miniProject5.addEventListener("click", ()=>{
-  window.location.href = "https://github.com/twaugh99/abc-student-repo/blob/master/projects/gift_project.zip";
-})
-
-miniProject6.addEventListener("click", ()=>{
-  window.location.href = "https://ipa-riddle.glitch.me/";
-})
-
-miniProject7.addEventListener("click", ()=>{
-  window.location.href = "https://simon-says-abc.glitch.me/";
-})
+let projects = [miniProject1, miniProject2, miniProject3, miniProject4, miniProject5, miniProject6, miniProject7, bigProject1, bigProject2, bigProject3];
+let projectsHREF = ["https://twaugh99.github.io/abc-student-repo/projects/multiplicity-project", "https://twaugh99.github.io/abc-student-repo/projects/time-project", "", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/bug-extension.zip", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/gift_project.zip", "https://ipa-riddle.glitch.me/", "https://simon-says-abc.glitch.me/", "https://twaugh99.github.io/abc-student-repo/projects/project-A", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/project-B/README.md", ""];
+let projectsInfo = ["Multiplicity Project  (9/9/21)", "Time Project  (9/9/21)", "", "Bug Extension (10/12/21)", "20-20-20 Extension (10/21/21)", "IPA Riddle (11/11/21)", "Simon Says (11/18/21)", "Project A  (9/30/21)", "SwipeySynth (10/27/21)", ""]
 
 
-bigProject1.addEventListener("click", ()=>{
-  window.location.href = "https://twaugh99.github.io/abc-student-repo/projects/project-A";
-})
+for(let i = 0; i < projects.length; i++){
+  console.log(i);
+  projects[i].addEventListener("click", ()=>{
+    window.location.href = projectsHREF[i];
+  })
 
-bigProject2.addEventListener("click", ()=>{
-  window.location.href = "https://github.com/twaugh99/abc-student-repo/blob/master/projects/project-B/README.md";
-})
+  projects[i].addEventListener("mouseenter", ()=>{
+    previewText.innerHTML = projectsInfo[i];
+  })
 
-bigProject3.addEventListener("click", ()=>{
-  // window.location.href = "";
-})
-
-
-
+  projects[i].addEventListener("mouseout", ()=>{
+    previewText.innerHTML = "";
+  })
+}
 
 //
-// function inputHappened(){
-//   // console.log(range.value);
-//   currentSelection = range.value;
+// miniProject1.addEventListener("click", ()=>{
+//   window.location.href = "https://twaugh99.github.io/abc-student-repo/projects/multiplicity-project";
+// })
 //
-//   // console.log(currentSelection);
+// miniProject2.addEventListener("click", ()=>{
+//   window.location.href = "https://twaugh99.github.io/abc-student-repo/projects/time-project";
+// })
 //
-//   document.getElementById('link').href = linkArray[currentSelection];
+// miniProject3.addEventListener("click", ()=>{
+//   // window.location.href = "";
+// })
 //
-//   // console.log("intended current link " + linkArray[currentSelection]);
-//   // console.log("current link " + document.getElementById('link').href);
+// miniProject4.addEventListener("click", ()=>{
+//   window.location.href = "https://github.com/twaugh99/abc-student-repo/blob/master/projects/bug-extension.zip";
+// })
 //
-//   if(currentSelection < 11){
-//     document.getElementById('link').innerHTML = "Mini Project " + currentSelection;
-//   } else {
-//     document.getElementById('link').innerHTML = "Big Project " + (currentSelection-10);
-//   }
+// miniProject5.addEventListener("click", ()=>{
+//   window.location.href = "https://github.com/twaugh99/abc-student-repo/blob/master/projects/gift_project.zip";
+// })
 //
-//   document.getElementById('icon').src = iconArray[currentSelection];
-//   document.getElementById('descriptionPTag').innerHTML = titleArray[currentSelection];
+// miniProject6.addEventListener("click", ()=>{
+//   window.location.href = "https://ipa-riddle.glitch.me/";
+// })
+//
+// miniProject7.addEventListener("click", ()=>{
+//   window.location.href = "https://simon-says-abc.glitch.me/";
+// })
 //
 //
-// }
+// bigProject1.addEventListener("click", ()=>{
+//   window.location.href = "https://twaugh99.github.io/abc-student-repo/projects/project-A";
+// })
+//
+// bigProject2.addEventListener("click", ()=>{
+//   window.location.href = "https://github.com/twaugh99/abc-student-repo/blob/master/projects/project-B/README.md";
+// })
+//
+// bigProject3.addEventListener("click", ()=>{
+//   // window.location.href = "";
+// })
+//
+//
+//
+// miniProject1.addEventListener("mouseenter", ()=>{
+//   previewText.innerHTML = "Multiplicity Project  (9/9/21)";
+// })
+//
+// miniProject1.addEventListener("mouseout", ()=>{
+//   previewText.innerHTML = "";
+// })
+miniProject1
+miniProject2
+miniProject3
+miniProject4
+miniProject5
+miniProject6
+miniProject7
+bigProject1
+bigProject2
+bigProject3
