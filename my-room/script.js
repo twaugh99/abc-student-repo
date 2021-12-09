1abe08453b3ff810e871031150cbcd90b5d38e9f
Thomas <tsw298@nyu.edu>
2021-12-09 09:23:11 -0600

    .

--- my-room/script.js
+++ my-room/script.js
@@ -3,8 +3,8 @@ console.log('script loaded');
 let previewText = document.getElementById("previewText");
 
 let projects = [miniProject1, miniProject2, miniProject3, miniProject4, miniProject5, miniProject6, miniProject7, bigProject1, bigProject2, bigProject3];
-let projectsHREF = ["https://twaugh99.github.io/abc-student-repo/projects/multiplicity-project", "https://twaugh99.github.io/abc-student-repo/projects/time-project", "", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/bug-extension.zip", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/gift_project.zip", "https://ipa-riddle.glitch.me/", "https://simon-says-abc.glitch.me/", "https://twaugh99.github.io/abc-student-repo/projects/project-A", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/project-B/README.md", "https://serverjam.glitch.me/"];
-let projectsInfo = ["Multiplicity Project  (9/9/21)", "Time Project  (9/9/21)", "", "Bug Extension (10/12/21)", "20-20-20 Extension (10/21/21)", "IPA Riddle (11/11/21)", "Simon Says (11/18/21)", "Project A  (9/30/21)", "SwipeySynth (10/27/21)", "ServerJam (12/7/21)"]
+let projectsHREF = ["https://twaugh99.github.io/abc-student-repo/projects/multiplicity-project", "https://twaugh99.github.io/abc-student-repo/projects/time-project", "https://twaugh99.github.io/abc-student-repo/projects/random_integrated_command_kompiler/", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/bug-extension.zip", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/gift_project.zip", "https://ipa-riddle.glitch.me/", "https://simon-says-abc.glitch.me/", "https://twaugh99.github.io/abc-student-repo/projects/project-A", "https://github.com/twaugh99/abc-student-repo/blob/master/projects/project-B/README.md", "https://serverjam.glitch.me/"];
+let projectsInfo = ["Multiplicity Project  (9/9/21)", "Time Project  (9/9/21)", "Browser Performance (9/23/21)", "Bug Extension (10/12/21)", "20-20-20 Extension (10/21/21)", "IPA Riddle (11/11/21)", "Simon Says (11/18/21)", "Project A  (9/30/21)", "SwipeySynth (10/27/21)", "ServerJam (12/7/21)"]
 
 
 for(let i = 0; i < projects.length; i++){
